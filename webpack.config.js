5de58227eb67756afc1569f5622bedc6f2ef716c
artemkata2211 <poxitivik@yandex.ru>
2025-06-18 13:50:04 +0500

    fgh

--- webpack.config.js
+++ webpack.config.js
@@ -1,117 +1,92 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { Template } = require('webpack');
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const loader = require('sass-loader');
-const GhPagesWebpackPlugin = require('gh-pages');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');s
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = (env) => {
-	return {
-		mode: env.mode ?? 'development',
-		entry: path.resolve(__dirname, 'src', 'main.js'),
-		output: {
-			path: path.resolve(__dirname, 'build'),
-			filename: '[name].[contenthash].js',
-			assetModuleFilename: 'assets/[name].[contenthash].[ext]',
-			clean: true
-		},
-		module: {
-			rules: [
-				{
-					test: /\.html$/i,
-					loader: 'html-loader'
-				},
-				{
-					test: /\.s[ac]ss$/i,
-					use: [
-						MiniCssExtractPlugin.loader,
-						'css-loader',
-						{
-							loader: 'postcss-loader',
-							options: {
-								postcssOptions: {
-									plugins: [
-										require('postcss-preset-env'),
-										autoprefixer({
-											overrideBrowserslist: ['ie >= 8', 'last 4 version']
-										})
-									]
-								}
-							}
-						},
-						'group-css-media-queries-loader',
-						'resolve-url-loader',
-						{ loader: 'sass-loader', options: { sourceMap: true } }
-					]
-				},
-				{
-					test: /\.(woff|woff2|eot|ttf|otf)$/i,
-					type: 'asset/resource',
-					generator: {
-						filename: 'fonts/[name][ext][query]',
-					}
-				},
-				{
-					test: /\.(jpe?g|png|webp|gif|svg)$/i,
-					use: [
-						{
-							loader: 'image-webpack-loader',
-							options: {
-								mozjpeg: {
-									progressive: true
-								},
-								optipng: {
-									enabled: false
-								},
-								pngquant: {
-									quality: [0.65, 0.9],
-									speed: 4
-								},
-								gifsicle: {
-									interlaced: false
-								},
-								webp: {
-									quality: 75
-								}
-							}
-						}
-					],
-					type: 'asset/resource',
-					 generator: {
-						filename: '[name].[ext][query]',
-        },
-				},
-				{
-					test: /\.(?:js|mjs|cjs)$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							targets: 'defaults',
-							presets: [['@babel/preset-env']]
-						}
-					}
-				}
-			]
-		},
+module.exports = {
+  // Входной файл
+  entry: [
+    './src/js/index.js'
+  ],
 
-		plugins: [
-			new HtmlWebpackPlugin({
-				template: path.resolve(__dirname, 'src', 'index.html')
-			}),
-			new webpack.ProgressPlugin(),
-			new MiniCssExtractPlugin({
-				filename: '[name].[contenthash].css'
-			})
-		],
-		devServer: {
-			static: {
-				directory: path.join(__dirname, 'src')
-			},
-			compress: true,
-			port: 9000
-		}
-	};
+  // Выходной файл
+  output: {
+    filename: './js/bundle.js'
+  },
+
+  // Source maps для удобства отладки
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      // Транспилируем js с babel
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      },
+
+      // Компилируем SCSS в CSS
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // Extract css to separate file
+          'css-loader', // translates CSS into CommonJS
+          'postcss-loader', // parse CSS and add vendor prefixes to CSS rules
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+
+      // Подключаем шрифты из css
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader?name=./fonts/[name].[ext]'
+          },
+        ]
+      },
+
+      // Подключаем картинки из css
+      {
+        test: /\.(svg|png|jpg|jpeg|webp)$/,
+        use: [
+          {
+            loader: 'file-loader?name=./static/[name].[ext]'
+          },
+        ]
+      },
+    ],
+  },
+  plugins: [
+    // Подключаем файл html, стили и скрипты встроятся автоматически
+    new HtmlWebpackPlugin({
+      title: 'Webpack 4 Starter',
+      template: './src/index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false,
+      }
+    }),
+
+    // Кладем стили в отдельный файлик
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+
+    // Копируем картинки
+    new CopyWebpackPlugin([
+      {
+        from: './src/img',
+        to: 'img',
+      },
+    ])
+  ],
 };
